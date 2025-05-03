b5fc88cbd9a462cab276fd9f1375857cca36f321
Josué David Hernández Ramírez <justinhernandez054@gmail.com>
2025-05-03 04:30:31 -0600

    feat: intialize implement theme management 🎨

---
+++ src/lib/components/ThemeToggle.svelte
@@ -0,0 +1,29 @@
+<script>
+    let isDarkMode = false;
+  
+    if (typeof window!== 'undefined') {
+      isDarkMode = localStorage.getItem('theme') === 'dark';
+      if (isDarkMode) {
+        document.documentElement.classList.add('dark');
+      }
+    }
+  
+    function toggleDarkMode() {
+      isDarkMode =!isDarkMode;
+      if (isDarkMode) {
+        document.documentElement.classList.add('dark');
+        localStorage.setItem('theme', 'dark');
+      } else {
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem('theme', 'light');
+      }
+    }
+  </script>
+  
+  <button on:click={toggleDarkMode}>
+    {#if isDarkMode}
+      Modo Claro
+    {:else}
+      Modo Oscuro
+    {/if}
+  </button>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,17 @@
+<svelte:head>
+  <script>
+    if (localStorage.getItem('theme') === 'dark') {
+      document.documentElement.classList.add('dark');
+    }
+  </script>
+</svelte:head>
+
 <script>
-	let { children } = $props();
-	import '../app.css';
+  let { children } = $props();
+  import ThemeToggle from '$lib/components/ThemeToggle.svelte';
+  import '../app.css';
 </script>
+
+<ThemeToggle />
 
 {@render children()}
