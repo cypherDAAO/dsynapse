15db9cf847ff1f4cf295d3bdbfc9f39c956ee617
Josué David Hernández Ramírez <justinhernandez054@gmail.com>
2025-05-03 02:52:47 -0600

    fix: corrections for compile 🪲

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,45 +1,38 @@
 <script lang="ts">
-    import type { HeliaLibp2p } from 'helia';
-    import type { Libp2p } from '@libp2p/interface';
-    import type { UnixFS } from '@helia/unixfs';
-    import { onMount } from 'svelte';
+	import type { HeliaLibp2p } from 'helia';
+	import type { Libp2p } from '@libp2p/interface';
+	import type { UnixFS } from '@helia/unixfs';
+	import { onMount } from 'svelte';
 
-    interface ipfsStruct {
-      node: HeliaLibp2p<any>;
-      fs: UnixFS;
-    }
-    let message: string = "Hello helia";
-    let cid: string;
-    let peerID: string;
+	interface ipfsStruct {
+		node: HeliaLibp2p<Libp2p<{ x: Record<string, unknown> }>>;
+		fs: UnixFS;
+	}
+	let message: string = 'Hello helia';
+	let cid: string;
+	let peerID: string;
 
-    onMount(async () => {
-            let ipfs: ipfsStruct | undefined;
-            let { default: initHelia } = await import('$lib/heliaModule')
-            const heliaNode = await initHelia();
-            if (heliaNode) {
-                ipfs = {
-                    node: heliaNode.node,
-                    fs: heliaNode.fs
-                };
-                (window as any).node = ipfs.node;
-                peerID = ipfs.node.libp2p.peerId.toString();
-                const cidTemp = await ipfs.fs.addBytes(new TextEncoder().encode(message));
-                cid = cidTemp.toString();
-            }
-            console.log('CID:', cid);
-            console.log('peerID:', peerID);
-    });
+	onMount(async () => {
+		let ipfs: ipfsStruct;
+		let { default: initHelia } = await import('$lib/heliaModule');
+		ipfs = await initHelia();
+		window.node = ipfs.node;
+		peerID = ipfs.node.libp2p.peerId.toString();
+		cid = await ipfs.fs.addBytes(new TextEncoder().encode(message));
+		//cid = cidTemp.toString();
+		console.log('CID:', cid);
+		console.log('peerID:', peerID);
+	});
 </script>
+
 <h1 class="text-3xl font-bold">IPFS DApp Manager</h1>
-<h3>message: { message }</h3>
-<h3>CID: { cid }</h3>
-<h3>peerID: { peerID }</h3>
+<h3>message: {message}</h3>
+<h3>CID: {cid}</h3>
+<h3>peerID: {peerID}</h3>
+
 <style lang="postcss">
-  @reference "tailwindcss";
-  :global(html) {
-    background-color: theme(--color-gray-100);
-  }
+	@reference "tailwindcss";
+	:global(html) {
+		background-color: theme(--color-gray-100);
+	}
 </style>
-
-
-
--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,11 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter({
+			fallback: 'index.html' // Esto hace que todas las rutas se sirvan con index.html
+		})
+	}
 };
 
 export default config;
